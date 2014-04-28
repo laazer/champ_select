fcc1b28db417c4de09d69b8db66c7856df2dac21
laazer <brandt.j@husky.neu.edu>
2014-04-28 08:17:58 -0400

    working urlmanager

--- src/com/laazer/common/UrlManager.java
+++ src/com/laazer/common/UrlManager.java
@@ -4,62 +4,30 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class UrlManager {
-    public static String excutePost(String targetURL, String urlParameters)
-    {
-      URL url;
-      HttpURLConnection connection = null;  
-      try {
-        //Create connection
-        url = new URL(targetURL);
-        connection = (HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", 
-             "application/x-www-form-urlencoded");
-              
-        connection.setRequestProperty("Content-Length", "" + 
-                 Integer.toString(urlParameters.getBytes().length));
-        connection.setRequestProperty("Content-Language", "en-US");  
-              
-        connection.setUseCaches (false);
-        connection.setDoInput(true);
-        connection.setDoOutput(true);
+    public static String executeGet(String target) {
+        String result = "";
+        try {
+           HttpsURLConnection con = (HttpsURLConnection)(new URL(target).openConnection());          
+            BufferedReader br = 
+                    new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
 
-        //Send request
-        DataOutputStream wr = new DataOutputStream (
-                    connection.getOutputStream ());
-        wr.writeBytes (urlParameters);
-        wr.flush ();
-        wr.close ();
+            String input;
 
-        //Get Response    
-        InputStream is = connection.getInputStream();
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-        String line;
-        StringBuffer response = new StringBuffer(); 
-        while((line = rd.readLine()) != null) {
-          response.append(line);
-          response.append('\r');
+            while ((input = br.readLine()) != null){
+                System.out.println(input);
+                result = result + input + "\n";
+            }
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            result = e.toString();
         }
-        rd.close();
-        return response.toString();
+        return result;
 
-      } catch (Exception e) {
-
-        e.printStackTrace();
-        return null;
-
-      } finally {
-
-        if(connection != null) {
-          connection.disconnect(); 
-        }
-      }
-    }
-    
-    public void main(String[] args) {
-       String target = "https://prod.api.pvp.net/api/lol/na/v1.4/summoner/by-name/InvParadox?api_key=f123e66a-3511-4219-bd29-71d8e315e603";
-       String result = UrlManager.excutePost(target, "");
-       System.out.println(result);
     }
 }
